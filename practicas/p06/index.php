7a79c137e4c24b6cbfae2bd0012bd81bdc5a1c20
RichardGz3 <ricardogzmt0@gmail.com>
2025-02-05 14:38:09 -0600

    Agregue primera funcion

---
+++ practicas/p06/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Práctica 6</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php include 'src/funciones.php'; ?>
+    
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p06/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    
+</body>
+</html>
