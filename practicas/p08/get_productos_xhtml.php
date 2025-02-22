2cda050a16dc826a9dc43ae6ff79408754df964b
RichardGz3 <ricardogzmt0@gmail.com>
2025-02-21 19:10:41 -0600

    Cree dos archivos, html y php, formulario y agregar productos a la BD

---
+++ practicas/p08/get_productos_xhtml.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<?php
+    $data = array();
+
+    /** SE CREA EL OBJETO DE CONEXION */
+    @$link = new mysqli('localhost', 'root', 'LGomr05RicML', 'marketzone');
+    
+    if ($link->connect_errno) {
+        die('Falló la conexión: '.$link->connect_error.'<br/>');
+    }
+
+    /** Consulta para obtener productos que no estén eliminados */
+    if ($result = $link->query("SELECT * FROM productos WHERE eliminado = 0")) {
+        $row = $result->fetch_all(MYSQLI_ASSOC);
+
+        foreach($row as $num => $registro) {
+            foreach($registro as $key => $value) {
+                $data[$num][$key] = utf8_encode($value);
+            }
+        }
+        
+        $result->free();
+    }
+
+    $link->close();
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Productos Vigentes</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+<body>
+    <h3>PRODUCTOS VIGENTES</h3>
+    <br/>
+    
+    <?php if( isset($row) ) : ?>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Marca</th>
+                    <th scope="col">Modelo</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Unidades</th>
+                    <th scope="col">Detalles</th>
+                    <th scope="col">Imagen</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($row as $value) : ?>
+                <tr>
+                    <th scope="row"><?= $value['id'] ?></th>
+                    <td><?= $value['nombre'] ?></td>
+                    <td><?= $value['marca'] ?></td>
+                    <td><?= $value['modelo'] ?></td>
+                    <td><?= $value['precio'] ?></td>
+                    <td><?= $value['unidades'] ?></td>
+                    <td><?= $value['detalles'] ?></td>
+                    <td><img src=<?= $value['imagen'] ?> ></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <p>No hay productos vigentes disponibles.</p>
+    <?php endif; ?>
+</body>
+</html>
