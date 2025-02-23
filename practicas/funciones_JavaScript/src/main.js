0f71a6b2247794303764b1159ce11346bafe07af
RichardGz3 <ricardogzmt0@gmail.com>
2025-02-22 23:28:24 -0600

    Agregue ejemplos de JS02

--- practicas/funciones_JavaScript/src/main.js
+++ practicas/funciones_JavaScript/src/main.js
@@ -3,3 +3,23 @@ function ejemplo1() {
     div.innerHTML = "<h3>Hola Mundo</h3>";
 }
 
+function ejemplo2() {
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+    
+    var resultadoDiv = document.getElementById('resultado2');
+    resultadoDiv.innerHTML = 
+    '<h3>Nombre: ' + nombre + '</h3>' +
+    '<h3>Edad: ' + edad + '</h3>' +
+    '<h3>Altura: ' + altura + '</h3>' +
+    '<h3>Casado: ' + casado + '</h3>';
+}
+
+function ejemplo3() {
+    var nombre = prompt("Ingresa tu nombre:", "");
+    var edad = prompt("Ingresa tu edad:", "");
+    
+    document.getElementById('resultado3').innerHTML = '<h3>Hola ' + nombre + ', así que tienes ' + edad + ' años.</h3>';
+}
